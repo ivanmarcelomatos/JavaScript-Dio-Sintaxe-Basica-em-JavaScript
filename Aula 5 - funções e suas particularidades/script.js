f27aaee0f2610574d13490071de8a70dcdd45896
ivanmarcelomatos <102636454+ivanmarcelomatos@users.noreply.github.com>
2022-06-22 20:42:22 -0300

    Adiciona slides e prática Aula 5 - Funções

--- Aula 5 - funções e suas particularidades/script.js
+++ Aula 5 - funções e suas particularidades/script.js
@@ -2,19 +2,23 @@
 
 // como declarar
 // deve ser declarada usando a palavra function seguida de "()" e "{}"
-/* function funcao() {
-    console.log('mensagem de uma função');
-}
+// function funcao() {
+//     console.log('mensagem escrita no escopo dentro de uma função');
+// }
 
-// chamando a função
-funcao(); */
+// // chamando a função
+// funcao();
 
-/* 
+
+
+
+
+
 // função com parâmetros
 // definindo parâmetros
-function mensagem(primeiro, segundo) {
-    console.log(primeiro, segundo)
-}
+// function dizQueSeAmam(primeiroNome, segundoNome) {
+//     console.log(primeiroNome + ' e ' + segundoNome + ' se amam.');
+// }
 
-// chamando a função atribuindo valor aos parâmetros
-mensagem('Tudo certo', 'jovem!'); */
+// // chamando a função atribuindo valor aos parâmetros
+// dizQueSeAmam('João', 'Maria'); 
