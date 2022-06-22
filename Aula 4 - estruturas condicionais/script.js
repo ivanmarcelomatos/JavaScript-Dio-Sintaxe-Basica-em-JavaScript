d7d8dc20f27dffcd53cc14d58bd6e5827d9a01b0
ivanmarcelomatos <102636454+ivanmarcelomatos@users.noreply.github.com>
2022-06-22 17:41:32 -0300

    praticando aula 4

--- Aula 4 - estruturas condicionais/script.js
+++ Aula 4 - estruturas condicionais/script.js
@@ -5,30 +5,53 @@
 
 
 
-var jogador1 = 1;
-var jogador2 = 3;
-var placar;
+// var jogador1 = 1;
+// var jogador2 = 3;
+// var placar;
 
 
 
 // // if
 
-// // aninhando if's
+
 // // if ternário
 // jogador1 != -1 && jogador2 != -1 ? console.log('os jogadores são válidos') : console.log('os jogadores não são válidos');
+// // jogador1 != -1 && jogador2 != -1 ? console.log('os jogadores são válidos') : console.log('os jogadores não são válidos');
+// // jogador1 != -1 && jogador2 != -1 ? console.log('os jogadores são válidos') : console.log('os jogadores não são válidos');
+// // jogador1 != -1 && jogador2 != -1 ? console.log('os jogadores são válidos') : console.log('os jogadores não são válidos');
+// // aninhando if's
 // if (jogador1 > 0 && jogador2 == 0) {
 //     console.log('o jogador 1 marcou ponto');
 //     placar = jogador1 > jogador2;
 // }
+// // if (jogador1 > 0 && jogador2 == 0) {
+// //     console.log('o jogador 1 marcou ponto');
+// //     placar = jogador1 > jogador2;
+// // }
+// // if (jogador1 > 0 && jogador2 == 0) {
+// //     console.log('o jogador 1 marcou ponto');
+// //     placar = jogador1 > jogador2;
+// // }
 // // usando o else if
 // else if (jogador2 > 0 && jogador1 == 0) {
 //     console.log('o jogador 2 marcou ponto');
 //     placar = jogador2 > jogador1;
 // }
+// // else if (jogador2 > 0 && jogador1 == 0) {
+// //     console.log('o jogador 2 marcou ponto');
+// //     placar = jogador2 > jogador1;
+// // }
+// // else if (jogador2 > 0 && jogador1 == 0) {
+// //     console.log('o jogador 2 marcou ponto');
+// //     placar = jogador2 > jogador1;
+// // }
 // // usando o else
 // else {
-//     console.log('ninguem marcou ponto');
+//     console.log('mensagem no else pois as condicionais anteriores não foram satisfeitas');
 // }
+// // else {
+// //     console.log('mensagem no else pois as condicionais anteriores não foram satisfeitas');
+// // }
 
 
 
@@ -36,9 +59,17 @@ var placar;
 
 
 
-// // usando switch case
+
+
+
+
+
+// var jogador1 = 1;
+// var jogador2 = 3;
 // placar = jogador2 > jogador1;
 // console.log(placar);
+
+// // usando switch case
 // switch (placar) {
 //     case placar = jogador1 > jogador2:
 //         console.log('jogador 1 ganhou');
@@ -47,8 +78,28 @@ var placar;
 //         console.log('jogador 2 ganhou');
 //         break;
 //     default:
-//         console.log('ninguem marcou ponto')
+//         console.log('aqui deve-se registrar a exceção não compreendida entre os cases');
 // }
+// // switch (placar) {
+// //     case placar = jogador1 > jogador2:
+// //         console.log('jogador 1 ganhou');
+// //         break;
+// //     case placar = jogador2 > jogador1:
+// //         console.log('jogador 2 ganhou');
+// //         break;
+// //     default:
+// //         console.log('aqui deve-se registrar a exceção não compreendida entre os cases'); 
+// // }
+// // switch (placar) {
+// //     case placar = jogador1 > jogador2:
+// //         console.log('jogador 1 ganhou');
+// //         break;
+// //     case placar = jogador2 > jogador1:
+// //         console.log('jogador 2 ganhou');
+// //         break;
+// //     default:
+// //         console.log('aqui deve-se registrar a exceção não compreendida entre os cases');
+// // }
 
 
 
@@ -58,16 +109,50 @@ var placar;
 
 
 
-// // estruturas de repetição
 
-// let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5']
-// let object = { propriedade1: '12587', propriedade2: 'valor2', propriedade3: 'valor3'}
+
+
+// estruturas de repetição
+
+
+
+let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5'];
+// let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5'];
+// let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5'];
+// let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5'];
+let object = {propriedade1: '12587', propriedade2: 'valor2', propriedade3: 'valor3'};
+// let object = {propriedade1: '12587', propriedade2: 'valor2', propriedade3: 'valor3'};
+// let object = { propriedade1: '12587', propriedade2: 'valor2', propriedade3: 'valor3'};
+// let objetc = { propriedade1: '12587', propriedade2: 'valor2', propriedade3: 'valor3'};
+console.log(array);
+console.log(object);
+// console.log(array);
+// console.log(object);
+// console.log(array);
+// console.log(object);
+// console.log(object);
+// console.log(object);
 
 
 // // for - executa até que seja false
 // for (let i = 0; i < array.length; i++) {
 //     console.log(i);
+// }
+// for (let i = 0; i < array.length; i++) {
+//     console.log(i);
+// }
+// for (let i = 0; i < array.length; i++) {
+//     console.log(i);
+// }
+// for (let i = 0; i < array.length; i++) {
+//     console.log(i);
+// }
+// for (let i = 0; i < array.length; i++) {
+//     console.log(i);
 // } 
+
+
+
 
 
 
@@ -78,12 +163,52 @@ var placar;
 // for (i in array) { //retnorna os índices
 //     console.log(i);
 // }
+// for (i in array) {
+//     console.log(i);
+// }
+// for (i in array) {
+//     console.log(i);
+// }
+// for (i in array) {
+//     console.log(i);
+// }
+// for (i in array){
+//     console.log(i);
+// }
+// for (i in array) {
+//     console.log(i);
+// }
+
+
+
+
+
 
 
 // // com object
-// for (i in object) { //retorna as propriedades
+// for (i in object) { //retorna os nomes das propriedades
 //     console.log(i);
 // }
+// for (i in object) {
+//     console.log(i);
+// }
+// for (i in object) {
+//     console.log(i);
+// }
+// for (i in object) {
+//     console.log(i);
+// }
+// for (i in object) {
+//     console.log(i);
+// }
+// for (i in object) {
+//     console.log(i);
+// }
+// for (i in object) {
+//     console.lgot(i);
+// }
+
+
 
 
 
@@ -95,38 +220,102 @@ var placar;
 // for (i of array) { //retorna os valores
 //     console.log(i);
 // }
-
-
-
-
-// // COM OBJECT NÃO FUNCIONA! POIS NÃO É ITERÁVEL
-// for (i of object.propriedade1) { // o retorno é fragmentando o valor
+// for (i of array) {
 //     console.log(i);
-// } 
-
-
-
-
-
-
-
-
-
-
-
-// // while - executa enquanto for verdadeira
-// var a = 0;
-// while (a < 10) {
-//     a++;
-//     console.log(a)
+// }
+// for (i of array) { 
+//     console.log(i);
+// }
+// for (i of array) {
+//     console.log(i);
+// }
+// for (i of array) {
+//     console.log(i);
+// }
+// for (i of array) {
+//     console.log(i);
+// }
+// for (i of array) {
+//     console.log(i);
 // }
 
 
 
 
-// // do - executa até que seja falsa
-// var a = 0;
-// do {
+
+
+// // COM OBJECT NÃO FUNCIONA! POIS NÃO É ITERÁVEL
+// for (i of object) { // erro pois não é iterável
+//     console.log(i);
+// }
+
+// // COM OBJECT DESSA FORMA FUNCIONA, MAS FRAGMENTA O VALOR EM LINHAS
+// for (i of object.propriedade2) { // o retorno é fragmentando o valor em linhas
+//     console.log(i);
+// }
+// for (i of object.propriedade2) {
+//     console.log(i);
+// }
+// for (i of object.propriedade1) {
+//     console.log(i);
+// }
+ 
+
+
+
+
+console.log('Daqui pra baixo é referente ao while');
+
+
+
+
+
+// while - executa enquanto for verdadeira
+var a = 0;
+console.log('valor inicial de a é igual a: ' + a);
+
+while (a < 10) {
+    a++;
+    console.log(a);
+}
+// while (a < 10) {
 //     a++;
 //     console.log(a);
-// } while (a < 10);
+// }
+// while (a < 10) {
+//     a++;
+//     console.log(a);
+// }
+// var a = 0;
+// while (a < 10) {
+//     a++;
+//     console.log(a);
+// }
+
+
+
+
+
+
+
+
+// do - executa até que seja falsa
+var b = 0;
+console.log('valor inicial de b é igual a: ' + b);
+
+do {
+    b++;
+    console.log(b);
+} while (b < 20);
+// do {
+//     b++;
+//     console.log(b);
+// } while (b < 20);
+// do {
+//     b++;
+//     console.log(b);
+// } while (b < 10);
+// do { 
+//     b++;
+//     console.log(b);
+// } while (b < 10);
